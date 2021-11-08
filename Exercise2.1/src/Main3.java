f9b65057d6acba18942ba2bf1e4df137d1364802
Cheryl0402 <yi.cheryl.chen@gmail.com>
2021-11-07 18:45:42 -0800

    add method statement to main 3, locked methods in JDisplay2

--- Exercise2.1/src/Main3.java
+++ Exercise2.1/src/Main3.java
@@ -11,12 +11,23 @@ public class Main3 {
     private static void addProc(HighLevelDisplay d) {
 
         // Add a sequence of addRow operations with short random naps.
+        for(int i = 0; i < 10; i++){
+            d.addRow("AAAAAAAAAA " + i);
+            d.addRow("BBBBBBBBBB " + i);
+            d.addRow("CCCC " + i);
+            nap(500);
+        }
 
     }
 
     private static void deleteProc(HighLevelDisplay d) {
 
         // Add a sequence of deletions of row 0 with short random naps.
+        for(int i = 0; i < 10; i++){
+            d.deleteRow(0);
+            nap(500);
+        }
+
     }
 
     public static void main(String [] args) {
